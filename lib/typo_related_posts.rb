e61451dc0feab751c4ca69b2f968d6eff6a8d24f
Frédéric de Villamil <frederic@de-villamil.com>
2009-04-14 23:30:51 +0200

    Refactoring, adds RSS support

--- lib/typo_related_posts.rb
+++ lib/typo_related_posts.rb
@@ -1,24 +1,13 @@
 # Copyright 2008 Frédéric de Villamil
 module Typorelatedposts
-  def display_related_posts
+  def get_related_posts
     if !@article.tags.empty?
     	mylist = Array.new
-    	@article.tags.each { |tag| 
+    	@article.tags.each do |tag| 
     		mylist += Tag.find_by_name(tag.name).articles
-    	}
-    mylist.uniq
-    tablo = mylist.sort_by {rand}[0,5]
-
-    html = '<div id="related">'
-    html += '<h3>À lire également</h3>'
-    html += '<p>Si cet article vous a intéressé, vous pouvez poursuivre votre lecture avec ces billets similaires :</p>'	
-    html += '<ul>'
-    tablo.each { |article|
-
-    html += "<li><a href='#{this_blog.base_url}#{article.permalink}'>#{article.title}</a></li>"
-    }
-    html += '</ul>'
-    html += '</div>'
+    	end
+      mylist.uniq
+      return mylist.sort_by {rand}[0,5]
     end
   end
 end
@@ -26,6 +15,27 @@ end
 module Typorelatedpostshelper
 
   def render_related_posts
-    @controller.display_related_posts
+    tablo = @controller.get_related_posts
+    
+    html = '<div id="related">'
+    html += '<h3>À lire également</h3>'
+    html += '<p>Si cet article vous a intéressé, vous pouvez poursuivre votre lecture avec ces billets similaires :</p>'	
+    html += '<ul>'
+    tablo.each do |article|
+      html += "<li><a href='#{this_blog.base_url}#{article.permalink}'>#{article.title}</a></li>"
+    end
+    html += '</ul>'
+    html += '</div>'
   end
+  
+  def rss_related_posts
+    tablo = @controller.get_related_posts
+    html = '<h3>À lire également</h3>'
+    html += '<ul>'
+    tablo.each do |article|
+      html += "<li><a href='#{this_blog.base_url}#{article.permalink}'>#{article.title}</a></li>"
+    end
+    html += '</ul>'
+  end
+  
 end
